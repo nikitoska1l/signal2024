7af0eb27e302bf0b657abbb73626a0d36aaca522
nikitoska1l <n.pischugin@ucar.pro>
2024-08-08 20:28:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const express = require('express');
 const TelegramBot = require('node-telegram-bot-api');
 const { google } = require('googleapis');
 const service_account_key = require('./service-account-key.json');
+const fs = require(fs);
 
 const app = express();
 const PORT = process.env.PORT || 10000;
