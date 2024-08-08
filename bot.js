d866d051a6dd4b924de2968ff09ba9e2d1b57b18
Nikita <n.pischugin@ucar.pro>
2024-08-08 18:18:18 +0300

    Update bot.js and add .env file

--- bot.js
+++ bot.js
@@ -6,110 +6,57 @@ const { google } = require('googleapis');
 const app = express();
 const PORT = process.env.PORT || 10000;
 
-const serviceAccountKey = JSON.parse(process.env.SERVICE_ACCOUNT_KEY);
+try {
+  const serviceAccountKey = JSON.parse(process.env.SERVICE_ACCOUNT_KEY);
 
-// Токен Telegram-бота из переменных окружения
-const TOKEN = process.env.TELEGRAM_BOT_TOKEN;
-const bot = new TelegramBot(TOKEN, { polling: true });
+  // Токен Telegram-бота из переменных окружения
+  const TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+  const bot = new TelegramBot(TOKEN, { polling: true });
 
-const client = new google.auth.JWT(
-  serviceAccountKey.client_email,
-  null,
-  serviceAccountKey.private_key,
-  ['https://www.googleapis.com/auth/spreadsheets']
-);
+  const client = new google.auth.JWT(
+    serviceAccountKey.client_email,
+    null,
+    serviceAccountKey.private_key,
+    ['https://www.googleapis.com/auth/spreadsheets']
+  );
 
-const sheets = google.sheets({ version: 'v4', auth: client });
-const SPREADSHEET_ID = process.env.SPREADSHEET_ID;
+  const sheets = google.sheets({ version: 'v4', auth: client });
+  const SPREADSHEET_ID = process.env.SPREADSHEET_ID;
 
-async function getSheetData(sheetName, range = 'A2:D') {
-  const request = {
-    spreadsheetId: SPREADSHEET_ID,
-    range: `${sheetName}!${range}`,
-  };
-
-  try {
-    const response = (await sheets.spreadsheets.values.get(request)).data;
-    return response.values || [];
-  } catch (err) {
-    console.error('The API returned an error: ' + err);
-    return [];
-  }
-}
-
-function getDateIndexMap(data) {
-  const dateIndexMap = {};
-  let currentDate = null;
-
-  data.forEach((row, index) => {
-    if (row[0]) {
-      currentDate = row[0];
-    }
-    if (!dateIndexMap[currentDate]) {
-      dateIndexMap[currentDate] = [];
-    }
-    dateIndexMap[currentDate].push(index);
-  });
-
-  return dateIndexMap;
-}
-
-bot.onText(/\/start/, (msg) => {
-  const chatId = msg.chat.id;
-  const welcomeMessage = "Привет, рейвер! Этот бот поможет тебе узнать расписание сетов на всех сценах Signalа. Выбери нужную сцену или получи инфо о событиях на территории.";
-
-  const options = {
-    reply_markup: {
-      inline_keyboard: [
-        [{ text: 'Сцены', callback_data: 'scenes' }],
-        [{ text: 'События', callback_data: 'events' }],
-      ],
-    },
-  };
-
-  bot.sendMessage(chatId, welcomeMessage, options);
-});
-
-bot.on('callback_query', async (callbackQuery) => {
-  const message = callbackQuery.message;
-  const data = callbackQuery.data;
-
-  if (data === 'scenes') {
-    const options = {
-      reply_markup: {
-        inline_keyboard: [
-          [{ text: 'Möbius', callback_data: 'Möbius' }],
-          [{ text: 'Meadow', callback_data: 'Meadow' }],
-          [{ text: 'Kiosko', callback_data: 'Kiosko' }],
-          [{ text: 'Signal', callback_data: 'Signal' }],
-          [{ text: 'Rodnya', callback_data: 'Rodnya' }],
-          [{ text: 'Ghosty', callback_data: 'Ghosty' }],
-          [{ text: 'Flower', callback_data: 'Flower' }],
-          [{ text: 'Prizma', callback_data: 'Prizma' }],
-          [{ text: 'Назад', callback_data: 'back_to_main' }],
-        ],
-      },
+  async function getSheetData(sheetName, range = 'A2:D') {
+    const request = {
+      spreadsheetId: SPREADSHEET_ID,
+      range: `${sheetName}!${range}`,
     };
-    bot.editMessageText('Выбери сцену:', { chat_id: message.chat.id, message_id: message.message_id, ...options });
-  } else if (data === 'events') {
-    const eventsData = await getSheetData('События');
-    const dateIndexMap = getDateIndexMap(eventsData);
-    const dateButtons = Object.keys(dateIndexMap).map(date => [{ text: date, callback_data: `event_date_${date}` }]);
 
-    bot.editMessageText('Выбери дату:', { chat_id: message.chat.id, message_id: message.message_id, reply_markup: { inline_keyboard: [...dateButtons, [{ text: 'Назад', callback_data: 'back_to_main' }]] } });
-  } else if (data.startsWith('event_date_')) {
-    const date = data.split('_')[2];
-    const eventsData = await getSheetData('События');
-    const dateIndexMap = getDateIndexMap(eventsData);
+    try {
+      const response = (await sheets.spreadsheets.values.get(request)).data;
+      return response.values || [];
+    } catch (err) {
+      console.error('The API returned an error: ' + err);
+      return [];
+    }
+  }
 
-    let response = `События - <b>${date}</b>:\n\n`;
-    dateIndexMap[date].forEach(index => {
-      const row = eventsData[index];
-      response += `<b>${row[1]}</b> - ${row[2]}\n<b>Место</b>: ${row[3]}\n\n`;
+  function getDateIndexMap(data) {
+    const dateIndexMap = {};
+    let currentDate = null;
+
+    data.forEach((row, index) => {
+      if (row[0]) {
+        currentDate = row[0];
+      }
+      if (!dateIndexMap[currentDate]) {
+        dateIndexMap[currentDate] = [];
+      }
+      dateIndexMap[currentDate].push(index);
     });
 
-    bot.editMessageText(response, { chat_id: message.chat.id, message_id: message.message_id, parse_mode: 'HTML', reply_markup: { inline_keyboard: [[{ text: 'Назад', callback_data: 'events' }]] } });
-  } else if (data === 'back_to_main') {
+    return dateIndexMap;
+  }
+
+  bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
     const welcomeMessage = "Привет, рейвер! Этот бот поможет тебе узнать расписание сетов на всех сценах Signalа. Выбери нужную сцену или получи инфо о событиях на территории.";
 
     const options = {
@@ -121,52 +68,123 @@ bot.on('callback_query', async (callbackQuery) => {
       },
     };
 
-    bot.editMessageText(welcomeMessage, { chat_id: message.chat.id, message_id: message.message_id, ...options });
-  } else if (data.startsWith('stage_')) {
-    const stage = data.split('_')[1];
-    const stageData = await getSheetData(stage);
+    bot.sendMessage(chatId, welcomeMessage, options);
+  });
 
-    if (stageData.length === 0) {
-      bot.editMessageText('Данные не найдены или произошла ошибка при получении данных.', { chat_id: message.chat.id, message_id: message.message_id, reply_markup: { inline_keyboard: [[{ text: 'Назад', callback_data: 'back_to_scenes' }]] } });
-      return;
+  bot.on('callback_query', async (callbackQuery) => {
+    const message = callbackQuery.message;
+    const data = callbackQuery.data;
+
+    if (data === 'scenes') {
+      const options = {
+        reply_markup: {
+          inline_keyboard: [
+            [{ text: 'Möbius', callback_data: 'Möbius' }],
+            [{ text: 'Meadow', callback_data: 'Meadow' }],
+            [{ text: 'Kiosko', callback_data: 'Kiosko' }],
+            [{ text: 'Signal', callback_data: 'Signal' }],
+            [{ text: 'Rodnya', callback_data: 'Rodnya' }],
+            [{ text: 'Ghosty', callback_data: 'Ghosty' }],
+            [{ text: 'Flower', callback_data: 'Flower' }],
+            [{ text: 'Prizma', callback_data: 'Prizma' }],
+            [{ text: 'Назад', callback_data: 'back_to_main' }],
+          ],
+        },
+      };
+      bot.editMessageText('Выбери сцену:', { chat_id: message.chat.id, message_id: message.message_id, ...options });
+    } else if (data === 'events') {
+      const eventsData = await getSheetData('События');
+      const dateIndexMap = getDateIndexMap(eventsData);
+      const dateButtons = Object.keys(dateIndexMap).map(date => [{ text: date, callback_data: `event_date_${date}` }]);
+
+      bot.editMessageText('Выбери дату:', { chat_id: message.chat.id, message_id: message.message_id, reply_markup: { inline_keyboard: [...dateButtons, [{ text: 'Назад', callback_data: 'back_to_main' }]] } });
+    } else if (data.startsWith('event_date_')) {
+      const date = data.split('_')[2];
+      const eventsData = await getSheetData('События');
+      const dateIndexMap = getDateIndexMap(eventsData);
+
+      let response = `События - <b>${date}</b>:\n\n`;
+      dateIndexMap[date].forEach(index => {
+        const row = eventsData[index];
+        response += `<b>${row[1]}</b> - ${row[2]}\n<b>Место</b>: ${row[3]}\n\n`;
+      });
+
+      bot.editMessageText(response, { chat_id: message.chat.id, message_id: message.message_id, parse_mode: 'HTML', reply_markup: { inline_keyboard: [[{ text: 'Назад', callback_data: 'events' }]] } });
+    } else if (data === 'back_to_main') {
+      const welcomeMessage = "Привет, рейвер! Этот бот поможет тебе узнать расписание сетов на всех сценах Signalа. Выбери нужную сцену или получи инфо о событиях на территории.";
+
+      const options = {
+        reply_markup: {
+          inline_keyboard: [
+            [{ text: 'Сцены', callback_data: 'scenes' }],
+            [{ text: 'События', callback_data: 'events' }],
+          ],
+        },
+      };
+
+      bot.editMessageText(welcomeMessage, { chat_id: message.chat.id, message_id: message.message_id, ...options });
+    } else if (data.startsWith('stage_')) {
+      const stage = data.split('_')[1];
+      const stageData = await getSheetData(stage);
+
+      if (stageData.length === 0) {
+        bot.editMessageText('Данные не найдены или произошла ошибка при получении данных.', { chat_id: message.chat.id, message_id: message.message_id, reply_markup: { inline_keyboard: [[{ text: 'Назад', callback_data: 'back_to_scenes' }]] } });
+        return;
+      }
+
+      const dateIndexMap = getDateIndexMap(stageData);
+      const dateButtons = Object.keys(dateIndexMap).map(date => [{ text: date, callback_data: `date_${stage}_${date}` }]);
+
+      bot.editMessageText('Выбери дату:', { chat_id: message.chat.id, message_id: message.message_id, reply_markup: { inline_keyboard: [...dateButtons, [{ text: 'Назад', callback_data: 'back_to_scenes' }]] } });
+    } else if (data.startsWith('date_')) {
+      const [_, stage, date] = data.split('_');
+      const stageData = await getSheetData(stage);
+      const dateIndexMap = getDateIndexMap(stageData);
+
+      let response = `<b>${stage}</b> - <b>${date}</b>:\n\n`;
+      dateIndexMap[date].forEach(index => {
+        const row = stageData[index];
+        response += `<b>${row[1]}</b> - ${row[2]}\n`;
+      });
+
+      bot.editMessageText(response, { chat_id: message.chat.id, message_id: message.message_id, parse_mode: 'HTML', reply_markup: { inline_keyboard: [[{ text: 'Назад', callback_data: `stage_${stage}` }]] } });
+    } else if (data === 'back_to_scenes') {
+      const options = {
+        reply_markup: {
+          inline_keyboard: [
+            [{ text: 'Möbius', callback_data: 'Möbius' }],
+            [{ text: 'Meadow', callback_data: 'Meadow' }],
+            [{ text: 'Kiosko', callback_data: 'Kiosko' }],
+            [{ text: 'Signal', callback_data: 'Signal' }],
+            [{ text: 'Rodnya', callback_data: 'Rodnya' }],
+            [{ text: 'Ghosty', callback_data: 'Ghosty' }],
+            [{ text: 'Flower', callback_data: 'Flower' }],
+            [{ text: 'Prizma', callback_data: 'Prizma' }],
+            [{ text: 'Назад', callback_data: 'back_to_main' }],
+          ],
+        },
+      };
+      bot.editMessageText('Выбери сцену:', { chat_id: message.chat.id, message_id: message.message_id, ...options });
+    } else {
+      const stage = data;
+      const stageData = await getSheetData(stage);
+
+      if (stageData.length === 0) {
+        bot.editMessageText('Данные не найдены или произошла ошибка при получении данных.', { chat_id: message.chat.id, message_id: message.message_id, reply_markup: { inline_keyboard: [[{ text: 'Назад', callback_data: 'back_to_scenes' }]] } });
+        return;
+      }
+
+      const dateIndexMap = getDateIndexMap(stageData);
+      const dateButtons = Object.keys(dateIndexMap).map(date => [{ text: date, callback_data: `date_${stage}_${date}` }]);
+
+      bot.editMessageText('Выбери дату:', { chat_id: message.chat.id, message_id: message.message_id, reply_markup: { inline_keyboard: [...dateButtons, [{ text: 'Назад', callback_data: 'back_to_scenes' }]] } });
     }
+  });
 
-    const dateIndexMap = getDateIndexMap(stageData);
-    const dateButtons = Object.keys(dateIndexMap).map(date => [{ text: date, callback_data: `date_${stage}_${date}` }]);
-
-    bot.editMessageText('Выбери дату:', { chat_id: message.chat.id, message_id: message.message_id, reply_markup: { inline_keyboard: [...dateButtons, [{ text: 'Назад', callback_data: 'back_to_scenes' }]] } });
-  } else if (data.startsWith('date_')) {
-    const [_, stage, date] = data.split('_');
-    const stageData = await getSheetData(stage);
-    const dateIndexMap = getDateIndexMap(stageData);
-
-    let response = `<b>${stage}</b> - <b>${date}</b>:\n\n`;
-    dateIndexMap[date].forEach(index => {
-      const row = stageData[index];
-      response += `<b>${row[1]}</b> - ${row[2]}\n`;
-    });
-
-    bot.editMessageText(response, { chat_id: message.chat.id, message_id: message.message_id, parse_mode: 'HTML', reply_markup: { inline_keyboard: [[{ text: 'Назад', callback_data: `stage_${stage}` }]] } });
-  } else if (data === 'back_to_scenes') {
-    const options = {
-      reply_markup: {
-        inline_keyboard: [
-          [{ text: 'Möbius', callback_data: 'Möbius' }],
-          [{ text: 'Meadow', callback_data: 'Meadow' }],
-          [{ text: 'Kiosko', callback_data: 'Kiosko' }],
-          [{ text: 'Signal', callback_data: 'Signal' }],
-          [{ text: 'Rodnya', callback_data: 'Rodnya' }],
-          [{ text: 'Ghosty', callback_data: 'Ghosty' }],
-          [{ text: 'Flower', callback_data: 'Flower' }],
-          [{ text: 'Prizma', callback_data: 'Prizma' }],
-          [{ text: 'Назад', callback_data: 'back_to_main' }],
-        ],
-      },
-    };
-    bot.editMessageText('Выбери сцену:', { chat_id: message.chat.id, message_id: message.message_id, ...options });
-  }
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+} catch (error) {
+  console.error("Service account key file not found or invalid:", error);
+  process.exit(1);
+}
